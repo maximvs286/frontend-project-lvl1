5ae6c626a89c05c9c7a2cd056f1680db86acc297
Maxim Serikov <maximserikov@Maxims-iMac.local>
2019-11-13 17:45:21 +0300

    Fine tune

--- Makefile
+++ Makefile
@@ -4,7 +4,10 @@ install:
 start:
 	npx babel-node src/bin/brain-games.js
 
-publish:
+prepublish:
 	npm publish --dry-run
+
+lint:
+	npx eslint
 
 .PHONY: test
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const readName = () => {
-    var userName = readlineSync.question('May I have your name? ');
+    let userName = readlineSync.question('May I have your name? ');
     console.log('Hello, ' + userName + '!');
 };
 
