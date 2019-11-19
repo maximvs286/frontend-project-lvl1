69b361a4de39f3d516d4f35cb3b18d845f4462ab
Maxim Serikov <maximserikov@Maxims-iMac.local>
2019-11-20 00:09:51 +0300

    correct calc

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ const generateData = (game) => {
         case 'even':
             return randomInteger(99);
         case 'calc':
-            return makeCalc(randomOperator(randomInteger(3)), randomInteger(5), randomInteger(5));
+            return makeCalc(randomOperator(randomInteger(3)), randomInteger(99), randomInteger(99));
     }
 };
 
