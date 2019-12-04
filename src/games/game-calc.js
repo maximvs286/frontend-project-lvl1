75b6657d659900d3be67b44a947c64c4e0a00a86
Maxim Serikov <maximserikov@Maxims-iMac.local>
2019-12-04 17:23:01 +0300

    refactor calc

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,45 +1,53 @@
-import { randomInteger, greeting, makeGameData, enterUserName, mainGame } from '../index';
+import { randomInteger, makeGameData, mainGame } from '../index';
+
+// rules string const
+
+const rulesStr = 'What is the result of the expression?\n';
+
+// random operator
+
+const randomOperatorStr = () => {
+    const randomNum = randomInteger(1, 3);
+    switch (randomNum) {
+        case 1:
+            return '-';
+        case 2:
+            return '+';
+        case 3:
+            return '*';
+    }
+};
+
+// find an answer
+
+const calcAnswerStr = (operatorStr, calcArg1, calcArg2) => {
+    switch (operatorStr) {
+        case '-':
+            return (calcArg1 - calcArg2).toString();
+        case '+':
+            return (calcArg1 + calcArg2).toString();
+        case '*':
+            return (calcArg1 * calcArg2).toString();
+    }
+};
+
+// generate game data
+
+const gameGenerator = () => {
+    const calcArgMin = 1;
+    const calcArgMax = 10; // '10' for easy mind calculate
+    const operatorStr = randomOperatorStr();
+    const calcArg1 = randomInteger(calcArgMin, calcArgMax);
+    const calcArg2 = randomInteger(calcArgMin, calcArgMax);
+
+    const questionStr = `${calcArg1} ${operatorStr} ${calcArg2}`;
+    const correctAnswerStr = calcAnswerStr(operatorStr, calcArg1, calcArg2);
+    
+    return makeGameData(questionStr, correctAnswerStr);
+};
+
+// game start
 
 export const startGame = () => {
-    const rules = 'What is the result of the expression?\n';
-
-    greeting(rules); // show greeting with rules
-
-    const userName = enterUserName();
-    
-    const gameGenerator = () => { // game data generator
-        //----------
-        const randomOperatorStr = (randomNum) => {
-            switch (randomNum) {
-                case 1:
-                    return '-';
-                case 2:
-                    return '+';
-                case 3:
-                    return '*';
-            }
-        };
-        const operatorMin = 1;
-        const operatorMax = 3;
-        const calcArgMin = 1;
-        const calcArgMax = 10;
-        const operatorStr = randomOperatorStr(randomInteger(operatorMin, operatorMax));
-        const calcArg1 = randomInteger(calcArgMin, calcArgMax);
-        const calcArg2 = randomInteger(calcArgMin, calcArgMax);
-        const calcAnswer = (operatorStr, calcArg1, calcArg2) => {
-            switch (operatorStr) {
-                case '-':
-                    return (calcArg1 - calcArg2).toString();
-                case '+':
-                    return (calcArg1 + calcArg2).toString();
-                case '*':
-                    return (calcArg1 * calcArg2).toString();
-            }
-        };
-        const correctAnswerStr = calcAnswer(operatorStr, calcArg1, calcArg2);
-        const questionStr = `${calcArg1} ${operatorStr} ${calcArg2}`;
-        //----------
-        return makeGameData(questionStr, correctAnswerStr); // game structure for engine
-    };
-    mainGame(userName, gameGenerator); // call main game cycle
+    mainGame(rulesStr, gameGenerator);
 };
