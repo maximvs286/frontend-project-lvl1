f1dc9b99381ef607ae6f53fd997584e20e6805b5
Maxim Serikov <maximserikov@Maxims-iMac.local>
2019-12-04 16:22:15 +0300

    refactor Even module

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,21 +1,23 @@
-import { randomInteger, greeting, makeGameData, enterUserName, mainGame } from '../index';
+import { randomInteger, makeGameData, mainGame } from '../index';
+
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".\n';
+
+const isEven = (num) => {
+    return num % 2 === 0 ? true : false;
+};
+
+const gameGenerator = () => { // game data generator
+    const min = 1;
+    const max = 99;
+
+    const question = randomInteger(min, max);
+    
+    const questionStr = question.toString();
+    const correctAnswerStr = isEven(question) ? 'yes' : 'no';
+    
+    return makeGameData(questionStr, correctAnswerStr); // game structure for engine
+};
 
 export const startGame = () => {
-    const rules = 'Answer "yes" if the number is even, otherwise answer "no".\n';
-
-    greeting(rules); // show greeting with rules
-
-    const userName = enterUserName();
-    
-    const gameGenerator = () => { // game data generator
-        //----------
-        const min = 1;
-        const max = 99;
-        const question = randomInteger(min, max);
-        const questionStr = question.toString();
-        const correctAnswerStr = question % 2 === 0 ? 'yes' : 'no';
-        //----------
-        return makeGameData(questionStr, correctAnswerStr); // game structure for engine
-    };
-    mainGame(userName, gameGenerator); // call main game cycle
+    mainGame(rules, gameGenerator); // call main game cycle
 };
