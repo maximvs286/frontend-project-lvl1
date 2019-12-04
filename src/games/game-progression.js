5c3e1d96cf558b77f4e4b77867350df326920663
Maxim Serikov <maximserikov@Maxims-iMac.local>
2019-12-04 18:19:56 +0300

    refactor progression

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,36 +1,45 @@
-import { randomInteger, greeting, makeGameData, enterUserName, mainGame } from '../index';
+import { randomInteger, makeGameData, mainGame } from '../index';
+
+// rules string const
+
+const rulesStr = 'What number is missing in the progression?\n';
+
+// calc a prog element
+
+const calcProgElement = (progStart, progIncrement, progPosition) => progIncrement * progPosition + progStart;
+
+// generate game data
+
+const gameGenerator = () => {
+    const progStartMin = 1;
+    const progStartMax = 90;
+    const progIncrementMin = 1;
+    const progIncrementMax = 10;
+    const progHiddenMin = 0;
+    const progHiddenMax = 9;
+    const progStart = randomInteger(progStartMin, progStartMax);
+    const progIncrement = randomInteger(progIncrementMin, progIncrementMax);
+    const progHidden = randomInteger(progHiddenMin, progHiddenMax);
+    
+    const startString = '';
+    const startAcc = 0;
+
+    const buildProgString = (acc, count) => {
+        const maxProgMember = 9;
+        if (count > maxProgMember) return acc;
+        if (count === progHidden) acc += '.. ';
+        else acc += `${calcProgElement(progStart, progIncrement, count)} `;
+        return buildProgString(acc, count + 1);
+    };
+
+    const questionStr = buildProgString(startString, startAcc);
+    const correctAnswerStr = calcProgElement(progStart, progIncrement, progHidden).toString();
+    
+    return makeGameData(questionStr, correctAnswerStr);
+};
+
+// game start
 
 export const startGame = () => {
-    const rules = 'What number is missing in the progression?\n';
-
-    greeting(rules); // show greeting with rules
-
-    const userName = enterUserName();
-
-    const gameGenerator = () => { // game data generator
-        //----------
-        const progStartMin = 1;
-        const progStartMax = 90;
-        const progIncrementMin = 1;
-        const progIncrementMax = 10;
-        const progHiddenMin = 0;
-        const progHiddenMax = 9;
-        const progStart = randomInteger(progStartMin, progStartMax);
-        const progIncrement = randomInteger(progIncrementMin, progIncrementMax);
-        const progHidden = randomInteger(progHiddenMin, progHiddenMax);
-        const correctAnswerStr = (progHidden * progIncrement + progStart).toString();
-        const buildProgString = (acc, count) => {
-            const maxProgMember = 9;
-            if (count > maxProgMember) return acc;
-            if (count === progHidden) acc += '.. ';
-            else acc += `${progStart + progIncrement * count} `;
-            return buildProgString(acc, count + 1);
-        };
-        const startString = '';
-        const startAcc = 0;
-        const questionStr = buildProgString(startString, startAcc);
-        //----------
-        return makeGameData(questionStr, correctAnswerStr); // game structure for engine
-    };
-    mainGame(userName, gameGenerator);
+    mainGame(rulesStr, gameGenerator);
 };
