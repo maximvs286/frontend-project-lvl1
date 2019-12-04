f72790aa6074677e1246a20dd602c9dd4a9075dc
Maxim Serikov <maximserikov@Maxims-iMac.local>
2019-12-04 17:53:53 +0300

    refactor gcd

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,26 +1,33 @@
-import { randomInteger, greeting, makeGameData, enterUserName, mainGame } from '../index';
+import { randomInteger, makeGameData, mainGame } from '../index';
+
+// rules string const
+
+const rulesStr = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
+
+// find an answer
+
+const isPrime = (num, count) => {
+    if (count === num) return true;
+    if (num % count === 0) return false;
+    return isPrime(num, count + 1);
+};
+
+// generate game data
+
+const gameGenerator = () => {
+    const primeMin = 1;
+    const primeMax = 99;
+    const prime = randomInteger(primeMin, primeMax);
+    const countStart = 2;
+
+    const questionStr = `${prime}`;
+    const correctAnswerStr = isPrime(prime, countStart) ? 'yes' : 'no';
+
+    return makeGameData(questionStr, correctAnswerStr);
+};
+
+// game start
 
 export const startGame = () => {
-    const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
-
-    greeting(rules); // show greeting with rules
-
-    const userName = enterUserName();
-    //----------
-    const gameGenerator = () => { // game data generator
-        const primeMin = 1;
-        const primeMax = 99;
-        const prime = randomInteger(primeMin, primeMax);
-        const questionStr = `${prime}`;
-        const isPrime = (num, count) => {
-            if (count === num) return 'yes';
-            if (num % count === 0) return 'no';
-            return isPrime(num, count + 1);
-        };
-        const countStart = 2;
-        const correctAnswerStr = isPrime(prime, countStart);
-    //----------
-        return makeGameData(questionStr, correctAnswerStr); // game structure for engine
-    };
-    mainGame(userName, gameGenerator);
+    mainGame(rulesStr, gameGenerator);
 };
