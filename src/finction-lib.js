4bcb6887ea329ab9f1e92c1996b8cf40f802bdbe
Maxim Serikov <maximserikov@Maxims-iMac.local>
2019-12-09 18:47:32 +0300

    add function-lib.js molule

---
+++ src/finction-lib.js
@@ -0,0 +1,23 @@
+//---------- randomize function ----------
+
+export const randomInteger = (min, max) => {
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+};
+
+//---------- structure for randomize function ----------
+
+export const makeGameData = (question, correctAnswer) => (message) => {
+    switch (message) {
+        case 'getQuestion':
+            return question;
+        case 'getCorrectAnswer':
+            return correctAnswer;
+    }
+};
+
+//---------- getters for structure ----------
+
+export const getQuestion = (gameData) => gameData('getQuestion');
+
+export const getCorrectAnswer = (gameData) => gameData('getCorrectAnswer');
--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 import readlineSync from 'readline-sync';
-
-//---------- randomize function ----------
-
-export const randomInteger = (min, max) => {
-    const rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
-};
-
-//---------- structure for randomize function ----------
-
-export const makeGameData = (question, correctAnswer) => (message) => {
-    switch (message) {
-        case 'getQuestion':
-            return question;
-        case 'getCorrectAnswer':
-            return correctAnswer;
-    }
-};
-
-//---------- getters ----------
-
-const getQuestion = (gameData) => gameData('getQuestion');
-
-const getCorrectAnswer = (gameData) => gameData('getCorrectAnswer');
+import { getQuestion, getCorrectAnswer } from './finction-lib'
 
 //---------- main cycle ----------
 
