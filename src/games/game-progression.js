aed758a952d541f88f8194e776ac7f10cc4c8766
Maxim Serikov <maximvs286@gmail.com>
2019-12-11 21:34:53 +0300

    refactor definitions

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,32 +1,30 @@
 import mainGame from '../index';
 import { randomInteger, makeGameData } from '../finction-lib';
 
-// rules string const
-
+// definitions
+const startMinValue = 1;
+const startMaxValue = 90;
+const incrementMinValue = 1;
+const incrementMaxValue = 10;
+const hiddenMinPosition = 1;
+const hiddenMaxPosition = 10;
 const gameTask = 'What number is missing in the progression?';
 
 // calc a prog element
 
-const calcProgressionElement = (progStart, progIncrement, progPosition) => progIncrement * progPosition + progStart;
+const calcProgressionElement = (startNum, increment, position) => increment * position + startNum;
 
 // generate game data
 
 const gererateGameData = () => {
-  const startMinValue = 1;
-  const startMaxValue = 90;
-  const incrementMinValue = 1;
-  const incrementMaxValue = 10;
-  const hiddenMinPosition = 0;
-  const hiddenMaxPosition = 9;
   const startNum = randomInteger(startMinValue, startMaxValue);
   const increment = randomInteger(incrementMinValue, incrementMaxValue);
   const hidden = randomInteger(hiddenMinPosition, hiddenMaxPosition);
   const startAcc = '';
-  const startCount = 0;
+  const startCount = 1;
 
-  const buildProgressionString = (acc, count) => {
-    const maxMember = 9; // min progresion number is '0'. so we have 10 elements
-    if (count > maxMember) return acc;
+  const buildProgressionString = (acc, count) => { 
+    if (count > hiddenMaxPosition) return acc;
     if (count === hidden) acc = `${acc} ..`;
     else acc = `${acc} ${calcProgressionElement(startNum, increment, count)}`;
     return buildProgressionString(acc, count + 1);
