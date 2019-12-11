2259cc78215516f465927153584b9c7c927f6b10
Maxim Serikov <maximvs286@gmail.com>
2019-12-11 19:21:14 +0300

    refactor definitions

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,8 +1,20 @@
 import mainGame from '../index';
 import { randomInteger, makeGameData } from '../finction-lib';
 
-// rules string const
+// definitions
 
+const startMinValue = 1;
+const startMaxValue = 90;
+const incrementMinValue = 1;
+const incrementMaxValue = 10;
+const hiddenMinValue = 0;
+const hiddenMaxValue = 9;
+const startNum = randomInteger(startMinValue, startMaxValue);
+const increment = randomInteger(incrementMinValue, incrementMaxValue);
+const hidden = randomInteger(hiddenMinValue, hiddenMaxValue);
+const startAcc = '';
+const startCount = 0;
+const maxMember = 9; // min progresion number is '0'. so we have 10 elements
 const gameTask = 'What number is missing in the progression?';
 
 // calc a prog element
@@ -12,26 +24,13 @@ const calcProgressionElement = (progStart, progIncrement, progPosition) => progI
 // generate game data
 
 const gererateGameData = () => {
-  const startMinValue = 1;
-  const startMaxValue = 90;
-  const incrementMinValue = 1;
-  const incrementMaxValue = 10;
-  const hiddenMinValue = 0;
-  const hiddenMaxValue = 9;
-  const startNum = randomInteger(startMinValue, startMaxValue);
-  const increment = randomInteger(incrementMinValue, incrementMaxValue);
-  const hidden = randomInteger(hiddenMinValue, hiddenMaxValue);
-  const startAcc = '';
-  const startCount = 0;
-
   const buildProgressionString = (acc, count) => {
-    const maxProgressionMember = 9;
-    if (count > maxProgressionMember) return acc;
+    if (count > maxMember) return acc;
     if (count === hidden) acc = `${acc} ..`;
     else acc = `${acc} ${calcProgressionElement(startNum, increment, count)}`;
     return buildProgressionString(acc, count + 1);
   };
-  
+
   const question = buildProgressionString(startAcc, startCount);
   const correctAnswer = calcProgressionElement(startNum, increment, hidden).toString();
   return makeGameData(question, correctAnswer);
