b49a728b46ccca44a230934cfaf57f2662e6fc63
Maxim Serikov <maximvs286@gmail.com>
2019-12-12 12:42:23 +0300

    add hexlet pair and refactor structure

--- src/finction-lib.js
+++ src/finction-lib.js
@@ -1,20 +1,12 @@
-//---------- randomize function ----------
+import { cons, car, cdr } from '@hexlet/pairs';
+
+// randomize function 
 export const randomInteger = (min, max) => {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 };
 
-//---------- structure for randomize function ----------
-export const makeGameData = (question, correctAnswer) => (message) => {
-  switch (message) {
-    case 'getQuestion':
-      return question;
-    case 'getCorrectAnswer':
-      return correctAnswer;
-  }
-};
-
-//---------- getters for structure ----------
-export const getQuestion = (gameData) => gameData('getQuestion');
-
-export const getCorrectAnswer = (gameData) => gameData('getCorrectAnswer');
+// structure for randomize function
+export const makeGameData = (quesion, correctAnswer) => cons(quesion, correctAnswer);
+export const getQuestion = gameData => car(gameData);
+export const getCorrectAnswer = gameData => cdr(gameData);
