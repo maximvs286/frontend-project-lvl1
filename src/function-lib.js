e920adfa3a24ae48c4becc8184d4f62ba8389e53
Maxim Serikov <maximvs286@gmail.com>
2019-12-12 15:07:04 +0300

    style with linter recomendation

--- src/function-lib.js
+++ src/function-lib.js
@@ -1,6 +1,6 @@
 import { cons, car, cdr } from '@hexlet/pairs';
 
-// randomize function 
+// randomize function
 export const randomInteger = (min, max) => {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
@@ -8,5 +8,5 @@ export const randomInteger = (min, max) => {
 
 // structure for randomize function
 export const makeGameData = (quesion, correctAnswer) => cons(quesion, correctAnswer);
-export const getQuestion = gameData => car(gameData);
-export const getCorrectAnswer = gameData => cdr(gameData);
+export const getQuestion = (gameData) => car(gameData);
+export const getCorrectAnswer = (gameData) => cdr(gameData);
