48e4546497e01712bd4a8db11e89324690ed7d44
Maxim Serikov <maximvs286@gmail.com>
2019-12-12 10:45:25 +0300

    style linespaces

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -2,19 +2,16 @@ import mainGame from '../index';
 import { randomInteger, makeGameData } from '../finction-lib';
 
 // definitions
-
 const minMumber = 1;
 const maxNumber = 99;
 const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // check even odd
-
 const isEven = (num) => {
   return num % 2 === 0 ? true : false;
 };
 
 // generate game data
-
 const gererateGameData = () => {
   const question = randomInteger(minMumber, maxNumber);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
@@ -22,7 +19,6 @@ const gererateGameData = () => {
 };
 
 // game start
-
 const startGame = () => {
   mainGame(gameTask, gererateGameData);
 };
