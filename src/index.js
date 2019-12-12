7675555ad6b6d7829fc7f08b970dacad4f59a99f
Maxim Serikov <maximvs286@gmail.com>
2019-12-12 13:02:53 +0300

    refactor mainGame

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import readlineSync from 'readline-sync';
 import { getQuestion, getCorrectAnswer } from './finction-lib';
 
+// definition
+const resetAcc = 0;
 const correctToEnd = 3; // correct game iterrations number
 
-//---------- main cycle ----------
-
+// main cycle
 const mainGame = (gameTask, gererateGameData) => {
   console.log(`\nWelcome to the Brain Games!\n${gameTask}\n`); // greeting function
   const userName = readlineSync.question('May I have your name? ');
@@ -13,7 +14,6 @@ const mainGame = (gameTask, gererateGameData) => {
 
   const mainGameIter = (gameAcc) => {
     if (gameAcc === correctToEnd) return console.log(`Congratulations, ${userName}!\n`);
-    const resetAcc = 0;
     const data = gererateGameData();
     const question = getQuestion(data);
     const correctAnswer = getCorrectAnswer(data);
@@ -23,7 +23,7 @@ const mainGame = (gameTask, gererateGameData) => {
       return mainGameIter(gameAcc + 1);
     }
     console.log(`\n'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!\n`);
-    return mainGameIter(resetAcc);
+    mainGameIter(resetAcc);
   };
     
   mainGameIter(gameAcc);
