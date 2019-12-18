522c6aa8828741e7fa3db47e1e0d58c4c43ed49e
Maxim Serikov <maximvs286@gmail.com>
2019-12-18 17:32:32 +0300

    refactor iter function

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -10,8 +10,8 @@ const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 // find an answer
 const isPrime = (num) => {
   const iter = (iterNum, count) => {
+    if (iterNum % count === 0 || iterNum < count) return false;
     if (count === iterNum) return true;
-    if (iterNum % count === 0) return false;
     return iter(iterNum, count + 1);
   };
 
