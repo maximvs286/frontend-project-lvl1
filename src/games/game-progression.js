388a3690f00c0345da2e57bd1a8c812e3c6ae9ad
Maxim Serikov <maximvs286@gmail.com>
2019-12-23 18:21:55 +0300

    style elemrntCounter

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -7,7 +7,7 @@ const incrementMinValue = 1;
 const incrementMaxValue = 10;
 const length = 10;
 const startAcc = '';
-const firstElement = 1;
+const elementCounter = 1;
 const gameTask = 'What number is missing in the progression?';
 
 const calcElement = (startNum, increment, position) => increment * position + startNum;
@@ -23,8 +23,8 @@ const composeQuestion = (acc, counter, startNum, increment, hidden) => {
 const gererateGameData = () => {
   const startNum = randomInteger(startMinValue, startMaxValue);
   const increment = randomInteger(incrementMinValue, incrementMaxValue);
-  const hidden = randomInteger(firstElement, length);
-  const question = composeQuestion(startAcc, firstElement, startNum, increment, hidden);
+  const hidden = randomInteger(elementCounter, length);
+  const question = composeQuestion(startAcc, elementCounter, startNum, increment, hidden);
   const correctAnswer = calcElement(startNum, increment, hidden).toString();
   return makeGameData(question, correctAnswer);
 };
