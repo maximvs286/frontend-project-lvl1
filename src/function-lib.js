305418c06b57cb4675d37d73133388d5531d83c4
Maxim Serikov <maximvs286@gmail.com>
2020-04-03 02:36:41 +0300

    update pairs

--- src/function-lib.js
+++ src/function-lib.js
@@ -1,4 +1,18 @@
-import { cons, car, cdr } from '@hexlet/pairs';
+// import { cons, car, cdr } from '@hexlet/pairs';
+
+const cons = (a, b) => (message) => {
+  switch (message) {
+    case 'car':
+      return a;
+    case 'cdr':
+      return b;
+    default:
+      return undefined;
+  }
+};
+
+const car = (pair) => pair('car');
+const cdr = (pair) => pair('cdr');
 
 export const randomInteger = (min, max) => {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
